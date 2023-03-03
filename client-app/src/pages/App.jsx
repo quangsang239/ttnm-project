a3a021acfd82474a3b5adf95bd9667fa020721e6
sangtq <sangtq@runsystem.net>
2023-03-03 13:38:40 +0700

    config router and tailwind css

--- client-app/src/pages/App.jsx
+++ client-app/src/pages/App.jsx
@@ -1,3 +1,7 @@
-function App() {}
+function App() {
+  return (
+    <div className="w-[100px] h-[100px] bg-slate-900 text-blue-500">hello</div>
+  );
+}
 export default App;
 
--- client-app/tailwind.config.cjs
+++ client-app/tailwind.config.cjs
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  corePlugins: {
-    preflight: false,
-  },
-  important: "#root",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
